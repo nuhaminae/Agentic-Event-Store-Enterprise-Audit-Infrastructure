83c1d26b40eb82ba5ff535ffb7d574c812db71fe
Nuhamin Alemayehu <nuhaminae@gmail.com>
2026-03-25 16:54:39 +0300

    feat(upcasting): add upcaster docstring

--- src/upcasting/registry.py
+++ src/upcasting/registry.py
@@ -8,9 +8,24 @@ from src.models.events import BaseEvent, StoredEvent
 
 class UpcasterRegistry:
     def __init__(self):
+        """Initialise UpcasterRegistry.
+
+        The registry is an empty dictionary at initialisation.
+        It will be populated with upcasters during registration.
+        """
+
         self._registry: dict[tuple[str, int], callable] = {}
 
     def register(self, event_type: str, from_version: int, upcaster):
+        
+        """
+        Register an upcaster for a given event type and version.
+
+        Args:
+            event_type (str): The event type to register the upcaster for.
+            from_version (int): The version of the event type to register the upcaster for.
+            upcaster (callable): A callable that takes a BaseEvent and returns an upcasted BaseEvent.
+        """
         self._registry[(event_type, from_version)] = upcaster
 
     def from_row(self, row) -> BaseEvent:
@@ -48,6 +63,13 @@ class UpcasterRegistry:
         return self.upcast(event)
 
     def upcast(self, event: BaseEvent) -> BaseEvent:
+        """Upcast an event to its latest version using registered upcasters.
+
+        The function iterates through the upcasters registered for the given event type
+        and version, applies each upcaster in order, and returns the upcasted event.
+        If no upcasters are registered for the given event type and version, the
+        event is returned as is.
+        """
         while (event.event_type, event.version) in self._registry:
             event = self._registry[(event.event_type, event.version)](event)
         return event
